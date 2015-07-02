dfb24649b43a723e09dd6ef31aa14c1b5ff9a6fb
philipslan <philiplan2017@u.northwestern.edu>
2015-07-02 18:54:57 -0400

    solutions

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
-$(document).ready(function(){ 
+$(document).on("click","#submit",function(){
 	var myDataRef = new Firebase('https://philiplan.firebaseio.com/');
-	$('#submit').on("click",function() {
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var subject = $('#subject').val();
-    var message= $('#message').val();
-    myDataRef.push({name: name, email: email, subject:subject, message: message});
- 	});
-}); 
-
-$(function(){
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 64
-        }, 1000);
-        return false;
-      }
-    }
-  });
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var subject = $('#subject').val();
+  var message= $('#message').val();
+  myDataRef.push({name: name, email: email, subject:subject, message: message});
 });
+
+$(document).on("click","a[href*=#]:not([href=#])",function(){
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 64
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+
+  
