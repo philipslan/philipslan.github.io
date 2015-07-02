b0ce89bc25cf9591db6068827b095be7deee928f
philipslan <philiplan2017@u.northwestern.edu>
2015-07-02 18:47:05 -0400

    new page

--- js/main.js
+++ js/main.js
@@ -1,42 +1,25 @@
-$(document).ready(function() 
-	{ 
-		$("#home a").on("click",function() {
-			event.preventDefault(); 
-			var target = $(this).attr("href"); 
-			$("html, body").stop().animate(
-				{ scrollLeft: 
-					$(target).offset().left, 
-					scrollTop: $(target).offset().top 
-				}, 500); 
-			$('#nav').fadeTo('fast', 0.1, function()
-			{
-			  $(this).css('visibility', 'visible');
-			}).fadeTo('slow', 1);
-		});
+$(document).ready(function(){ 
+	var myDataRef = new Firebase('https://philiplan.firebaseio.com/');
+	$('#submit').on("click",function() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message= $('#message').val();
+    myDataRef.push({name: name, email: email, subject:subject, message: message});
+ 	});
+}); 
 
-		$("#nav #homenav").on("click", function() {
-			$("#nav").css('visibility','hidden');
-		});
-
-		$("#nav a").bind("click",function(event)
-			{ 
-				event.preventDefault(); 
-				var target = $(this).attr("href"); 
-				$("html, body").stop().animate(
-					{ scrollLeft: 
-						$(target).offset().left, 
-						scrollTop: $(target).offset().top 
-					}, 500); 
-
-			});
-
-		var myDataRef = new Firebase('https://philiplan.firebaseio.com/');
-		$('#submit').on("click",function() {
-      var name = $('#name').val();
-      var email = $('#email').val();
-      var subject = $('#subject').val();
-      var message= $('#message').val();
-      myDataRef.push({name: name, email: email, subject:subject, message: message});
-   	});
-	}); 
-
+$(function(){
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 64
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
