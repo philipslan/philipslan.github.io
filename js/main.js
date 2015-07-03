a49365c07b5ba0b3d4d6253c8254e0494de1d2e0
philipslan <philiplan2017@u.northwestern.edu>
2015-07-03 15:54:37 -0400

    mandrill

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
-  myDataRef = new Firebase('https://philiplan.firebaseio.com/');
+  $('.modal-trigger').leanModal();
 });
 
 $(document).on("click","#submit",function(){
   var name = $('#name').val();
   var email = $('#email').val();
   var message= $('#message').val();
-  myDataRef.push({name: name, email: email, message: message});
+  $.ajax({
+    type: 'POST',
+    url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+    data: {
+      'key': '8ay6DleghB_atygG95SOYA',
+      'message': {
+        'from_email': 'philipslan@gmail.com',
+        'to':[
+          {
+            'email':'philipslan@gmail.com',
+            'name': name,
+            'type': 'to'
+          }
+        ],
+        'autotext': 'true',
+        'subject': 'Website',
+        'html': message + ' Email:' + email
+      }
+    }
+  }).done(function(response) {
+    console.log(response); // if you're into that sorta thing
+  });
 });
 
 $(document).on("click","a[href*=#]:not([href=#])",function(){
